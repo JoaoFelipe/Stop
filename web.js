5c49fc66177a3f1b8356aaee8ec8784cfcb5ba63
JoaoFelipe <joaofelipenp@gmail.com>
2012-12-03 14:07:06 -0600

    checking what occurs when the node crash

--- web.js
+++ web.js
@@ -33,7 +33,7 @@ app.configure(function () {
      
 });
 
-var s = app.listen(process.env.PORT || 8080);
+var s = app.listen(process.env.PORT || 5000);
 
 server = io.listen(s).set( 'log level', 1 );
 
@@ -105,7 +105,11 @@ server.sockets.on( 'connection', function( socket ) {
     chat_inside(socket);
 
     socket.on('debug', function(data){
-        socket.emit('debug', {context: socket.context, user:socket.user})
+        socket.emit('debug', {context: socket.context, user:socket.user});
+    });
+
+    socket.on('crash', function(data){
+        asdasd
     });
 
 });
